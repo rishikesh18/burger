0edc9885047233056eb9db4a2e5759afb2aca8e5
rishikesh18 <rishi.neupane@gmail.com>
2018-06-04 13:04:25 -0400

    added burgers.js in assets

--- models/burger.js
+++ models/burger.js
@@ -3,23 +3,23 @@ var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.all(".........", function(res) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("..........", cols, vals, function(res) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("..........", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("..........", condition, function(res) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
